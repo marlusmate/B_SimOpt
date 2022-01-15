0467d08f28e4ff96e656877714eac6c541aeefc1
marlusmate <93728218+marlusmate@users.noreply.github.com>
2022-01-15 12:58:05 +0100

    Add files via upload

---
+++ Scripte/getMetrics.py
@@ -0,0 +1,11 @@
+import json
+
+with open('metrics.json') as file:
+    temp = json.load(file)
+
+with open('../metrics.json', 'w+') as out:
+    json.dump(temp, out)
+
+#print(dix)
+#print('Dateityp: ', type(dix))
+
